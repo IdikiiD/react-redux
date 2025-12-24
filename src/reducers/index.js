72e044fe2cdd550c4d613fe3abc172be13e78b74
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-24 17:08:04 +0200

    add filters loading status management with fetching actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -40,7 +40,26 @@ const reducer = (state = initialState, action) => {
                     newCreatedHeroes.filter(item => item.element === state.activeFilter)
             }
 
-            default: return state
+        case'FILTERS_FETCHING':
+            return {
+                ...state,
+                filtersLoadingStatus: 'loading'
+            }
+        case 'FILTERS_FETCHED':
+            return {
+                ...state,
+                filters: action.payload,
+                filtersLoadingStatus: 'idle'
+            }
+        case 'FILTERS_FETCHING_ERROR':
+            return {
+                ...state,
+                filtersLoadingStatus: 'error'
+
+            }
+
+        default:
+            return state
 
     }
 }
