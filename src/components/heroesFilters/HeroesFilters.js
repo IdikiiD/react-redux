d5a228ab0a26ef353eeee9fc8c3f84b001bc4ca0
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-24 17:34:08 +0200

    fix:add new hero "Ivan Dikiivv" to heroes.json and clean up HeroesFilters.js

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -29,7 +29,6 @@ const HeroesFilters = () => {
             .then(data => dispatch(filtersFetched(data)))
             .catch(() => dispatch(filtersFetchingError()))
 
-        // eslint-disable-next-line
     }, []);
 
     const renderFilters = (data) => {
@@ -42,11 +41,7 @@ const HeroesFilters = () => {
     }
 
 
-    // <button className="btn btn-outline-dark active">Все</button>
-    // <button className="btn btn-danger">Огонь</button>
-    // <button className="btn btn-primary">Вода</button>
-    // <button className="btn btn-success">Ветер</button>
-    // <button className="btn btn-secondary">Земля</button>
+
 
 
     return (
