9eba9091aced290060dd47091f2cff503526aa48
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-24 10:20:57 +0200

    add action creator for adding a new hero

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,3 +16,10 @@ export const heroesFetchingError = () => {
         type: 'HEROES_FETCHING_ERROR'
     }
 }
+
+export const addHerro  = (hero) => {
+    return {
+        type: 'ADD_HERO',
+         payload: hero
+    }
+}
