135c5df4c00ec3e6321ec5a5c144ade8deafe209
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-24 17:07:53 +0200

    initialize project structure with basic components and setup

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,4 +1,3 @@
-
 // Задача для этого компонента:
 // Фильтры должны формироваться на основании загруженных данных
 // Фильтры должны отображать только нужных героев при выборе
@@ -6,17 +5,54 @@
 // Изменять json-файл для удобства МОЖНО!
 // Представьте, что вы попросили бэкенд-разработчика об этом
 
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
+import {
+    filtersFetched,
+    filtersFetching,
+    filtersFetchingError,
+} from "../../actions";
+
+import {useDispatch} from "react-redux";
+import {useHttp} from "../../hooks/http.hook";
+
+
 const HeroesFilters = () => {
+    const {activeFilter, filters,filtersLoadingStatus} = useSelector(state => state);
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+
+    useEffect(() => {
+        dispatch(filtersFetching());
+        request("http://localhost:3001/filters")
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()))
+
+        // eslint-disable-next-line
+    }, []);
+
+    const renderFilters = (data) => {
+        if (data.length > 0) {
+            return data.map(filter => {
+                return <button className="btn btn-outline-dark active" key={filter} value={filter}>{filter}</button>
+            })
+        }
+    }
+
+
+    // <button className="btn btn-outline-dark active">Все</button>
+    // <button className="btn btn-danger">Огонь</button>
+    // <button className="btn btn-primary">Вода</button>
+    // <button className="btn btn-success">Ветер</button>
+    // <button className="btn btn-secondary">Земля</button>
+
+
     return (
         <div className="card shadow-lg mt-4">
             <div className="card-body">
                 <p className="card-text">Отфильтруйте героев по элементам</p>
                 <div className="btn-group">
-                    <button className="btn btn-outline-dark active">Все</button>
-                    <button className="btn btn-danger">Огонь</button>
-                    <button className="btn btn-primary">Вода</button>
-                    <button className="btn btn-success">Ветер</button>
-                    <button className="btn btn-secondary">Земля</button>
+                    {renderFilters(filters)}
                 </div>
             </div>
         </div>
