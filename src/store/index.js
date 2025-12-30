0c462e61e5d5bbbac325614ff813459660e95fc3
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-30 16:50:13 +0200

    feat: refactor store setup to use combined reducers for heroes and filters

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux';
-import reducer from '../reducers';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import heroes from '../reducers/hero.js';
+import filters from '../reducers/filter.js';
+
+import {combineReducers} from "@reduxjs/toolkit";
+
+
+const store = createStore(combineReducers({heroes,filters}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
