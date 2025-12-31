1075bb59c6851a338458d695b75bf61a672fea44
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-31 06:30:53 +0200

    feat: enhance store setup with custom dispatch for string actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import heroes from '../reducers/hero.js';
 import filters from '../reducers/filter.js';
 
 import {combineReducers} from "@reduxjs/toolkit";
 
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const oldDispatch = store.dispatch;
 
-const store = createStore(combineReducers({heroes,filters}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    store.dispatch = (action) => {
+        if(typeof action === 'string') {
+            return oldDispatch({type: action});
+        }
+        return oldDispatch(action);
+    }
+    return store;
+
+};
+
+
+const store = createStore(
+    combineReducers({heroes,filters}),compose(enhancer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ));
 
 export default store;
+
