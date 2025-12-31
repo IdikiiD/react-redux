8897d27466c23f2c95af796638decea88ccb1ddf
Ivan <128108289+IdikiiD@users.noreply.github.com>
2025-12-31 18:00:43 +0200

    feat: update store setup to include thunk middleware and streamline string action handling

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,37 @@
-import { createStore, compose } from 'redux';
-
+import {createStore, compose} from 'redux';
+import { thunk } from 'redux-thunk'
 import heroes from '../reducers/hero.js';
 import filters from '../reducers/filter.js';
+
 
 import {applyMiddleware, combineReducers} from "@reduxjs/toolkit";
 
 const stringMiddleware = () => (next) => (action) => {
-    if(typeof action === 'string') {
-        return dispatch({type: action});
+    if (typeof action === 'string') {
+        return next({type: action});
     }
-    return dispatch(action);
+    return next(action);
 }
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-    const oldDispatch = store.dispatch;
-
-    store.dispatch = (action) => {
-        if(typeof action === 'string') {
-            return oldDispatch({type: action});
-        }
-        return oldDispatch(action);
-    }
-    return store;
-
-};
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//     const oldDispatch = store.dispatch;
+//
+//     store.dispatch = (action) => {
+//         if(typeof action === 'string') {
+//             return oldDispatch({type: action});
+//         }
+//         return oldDispatch(action);
+//     }
+//     return store;
+//
+// };
 
 
 const store = createStore(
-    combineReducers({heroes,filters}),
+    combineReducers({heroes, filters}),
     compose(
-    applyMiddleware(stringMiddleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk, stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     ));
 
 export default store;
