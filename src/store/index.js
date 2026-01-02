3f36001de56f9906aacbeafeef8bcb617df0c5f5
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-02 19:17:00 +0200

    feat: refactor store setup to use configureStore for improved middleware handling

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import heroes from '../reducers/hero.js';
 import filters from '../reducers/filter.js';
 
 
-import {applyMiddleware, combineReducers} from "@reduxjs/toolkit";
+import {applyMiddleware, combineReducers, configureStore} from "@reduxjs/toolkit";
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
@@ -28,10 +28,12 @@ const stringMiddleware = () => (next) => (action) => {
 // };
 
 
-const store = createStore(
-    combineReducers({heroes, filters}),
-    compose(
-        applyMiddleware(thunk, stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ));
+
+
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
