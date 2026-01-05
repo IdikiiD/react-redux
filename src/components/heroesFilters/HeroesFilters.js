f258588378a98a9b3731d9e4464af30ef339a0f0
Ivan <128108289+IdikiiD@users.noreply.github.com>
2026-01-05 20:31:57 +0200

    feat: enhance HeroesFilters to utilize selectAll for filter retrieval

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,22 +1,22 @@
-
-
 import {useSelector} from "react-redux";
 import {useEffect} from "react";
 
-import {activeFilterChanged, fetchFilters} from "./filterSlice";
+import {activeFilterChanged, fetchFilters, selectAll} from "./filterSlice";
 
 import {useDispatch} from "react-redux";
 import {useHttp} from "../../hooks/http.hook";
 import classNames from "classnames";
+import store from "../../store";
 
 
 const HeroesFilters = () => {
-    const {activeFilter, filters, filtersLoadingStatus} = useSelector(state => state.filters);
+    const {activeFilter, filtersLoadingStatus} = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
     const dispatch = useDispatch();
     const {request} = useHttp();
 
     useEffect(() => {
-dispatch(fetchFilters())
+        dispatch(fetchFilters())
 
     }, []);
 
@@ -24,13 +24,12 @@ dispatch(fetchFilters())
         if (data.length > 0) {
             return data.map(({name, className, label}) => {
                 const btnClass = classNames("btn", className, {'active': name === activeFilter});
-                return <button className={btnClass} key={name} id={name} onClick={()=>{dispatch(activeFilterChanged(name))}}>{label}</button>
+                return <button className={btnClass} key={name} id={name} onClick={() => {
+                    dispatch(activeFilterChanged(name))
+                }}>{label}</button>
             })
         }
     }
-
-
-
 
 
     return (
